76e6d40a8108754c909c3bd393e7a7e7230a622c
karim ANNOUN <karim.announ@g.enp.edu.dz>
2023-08-12 16:09:28 +0100

    pg

--- express/index.js
+++ express/index.js
@@ -57,7 +57,8 @@ app.use(
     cookie:{  
       expires:Date.now() + 1000 * 60 * 60 * 24 * 7 ,
       maxAge: 1000 * 60 * 60 * 24 * 7 ,
-      domain:'https://successful-outerwear-ray.cyclic.app'
+      domain:'https://instaprojectnew.netlify.app/',
+     
       
      // httpOnly: true, 
      // secure: true, 
