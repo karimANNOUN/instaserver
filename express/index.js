e74b7c26ab83c12dfc240e91c0691fca61f89e74
karim ANNOUN <karim.announ@g.enp.edu.dz>
2023-08-12 16:04:13 +0100

    pp

--- express/index.js
+++ express/index.js
@@ -57,6 +57,7 @@ app.use(
     cookie:{  
       expires:Date.now() + 1000 * 60 * 60 * 24 * 7 ,
       maxAge: 1000 * 60 * 60 * 24 * 7 ,
+      domain:'https://successful-outerwear-ray.cyclic.app'
       
      // httpOnly: true, 
      // secure: true, 
