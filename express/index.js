4e3c047646a72ee075f9c56db51c4829536bc9e1
karim ANNOUN <karim.announ@g.enp.edu.dz>
2023-08-13 20:10:20 +0100

    horn

--- express/index.js
+++ express/index.js
@@ -65,10 +65,10 @@ app.use(
     cookie:{  
       expires:Date.now() + 1000 * 60 * 60 * 24 * 7 ,
       maxAge: 1000 * 60 * 60 * 24 * 7 ,
-     sameSite:'none',
+   //  sameSite:'none',
       
     //  httpOnly: false, 
-      secure: true, 
+    //  secure: true, 
       }
   })
 )
