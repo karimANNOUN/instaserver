4249c41fd4cd11dba156b25df1eb8da29d403efc
karim ANNOUN <karim.announ@g.enp.edu.dz>
2023-08-13 20:40:39 +0100

    rhhtyt

--- express/index.js
+++ express/index.js
@@ -65,7 +65,7 @@ app.use(
     cookie:{  
       expires:Date.now() + 1000 * 60 * 60 * 24 * 7 ,
       maxAge: 1000 * 60 * 60 * 24 * 7 ,
-      domain:'instaprojectnew.netlify.app'
+      sameSite:false
       
     //  httpOnly: false, 
     //  secure: true, 
