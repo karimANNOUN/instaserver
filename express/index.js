ef4ff6db00bff87735177f23456c352bc2b43c1a
karim ANNOUN <karim.announ@g.enp.edu.dz>
2023-08-13 06:30:08 +0100

    domain

--- express/index.js
+++ express/index.js
@@ -65,7 +65,7 @@ app.use(
     cookie:{  
       expires:Date.now() + 1000 * 60 * 60 * 24 * 7 ,
       maxAge: 1000 * 60 * 60 * 24 * 7 ,
-      domain:'https://instaprojectnew.netlify.app/',
+      
      
       
      // httpOnly: true, 
