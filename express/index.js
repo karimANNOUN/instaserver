f0922818c5382ea0c9e75ff487a89bba4d2ba0f0
karim ANNOUN <karim.announ@g.enp.edu.dz>
2023-08-13 11:37:11 +0100

    ttrbh

--- express/index.js
+++ express/index.js
@@ -65,7 +65,7 @@ app.use(
     cookie:{  
       expires:Date.now() + 1000 * 60 * 60 * 24 * 7 ,
       maxAge: 1000 * 60 * 60 * 24 * 7 ,
-      path:''
+      path:'/'
      
      // httpOnly: true, 
      // secure: true, 
